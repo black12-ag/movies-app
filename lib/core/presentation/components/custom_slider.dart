47a09c5cf9636614a69038d7c027fbce314df6e1
Hammad Ali <hammadaali02@gmail.com>
2024-08-12 16:51:13 +0500

    solving errors

--- lib/core/presentation/components/custom_slider.dart
+++ lib/core/presentation/components/custom_slider.dart
@@ -1,7 +1,9 @@
+
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-import 'package:movies_app/core/resources/app_constants.dart';
+import '../../resources/app_constants.dart';
 
 class CustomSlider extends StatelessWidget {
   final Widget Function(BuildContext context, int itemIndex, int) itemBuilder;
