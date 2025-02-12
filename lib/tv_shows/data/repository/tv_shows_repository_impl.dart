c628153298a0eee53cf0b77bb0e51d1d391c0785
Hammad Ali <hammadaali02@gmail.com>
2025-02-12 22:50:07 +0500

    fix: remove unused import in TVShowsRepositoryImpl

--- lib/tv_shows/data/repository/tv_shows_repository_impl.dart
+++ lib/tv_shows/data/repository/tv_shows_repository_impl.dart
@@ -4,7 +4,7 @@ import 'package:movies_app/core/domain/entities/media.dart';
 import 'package:movies_app/core/domain/entities/media_details.dart';
 import 'package:movies_app/tv_shows/data/datasource/tv_shows_remote_data_source.dart';
 import 'package:movies_app/tv_shows/domain/entities/season_details.dart';
-import 'package:movies_app/core/data/error/failure.dart';
+// import 'package:movies_app/core/data/error/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:movies_app/tv_shows/domain/repository/tv_shows_repository.dart';
 import 'package:movies_app/tv_shows/domain/usecases/get_season_details_usecase.dart';
